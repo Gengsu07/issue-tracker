3a628330f09060e7c5d2d4c43bda78a7d6c83b48
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-10-15 19:50:20 +0700

    add new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import React from "react";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Input radius="large" placeholder="Issue title" />
+      <TextArea variant="surface" size={"3"} placeholder="Issue description" />
+      <Button variant="solid">Submit Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuesPage = () => {
   return (
     <div>
       <h1>IssuesPage</h1>
-      <Button>Add Issue</Button>
+      <Button>
+        <Link href={"/issues/new"}>New Issue</Link>
+      </Button>
     </div>
   );
 };
