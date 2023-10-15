2d8228f9e37ad09ce0e97a5a33244fa7f92c05de
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-10-15 20:18:07 +0700

    customize radix ui

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,7 +7,9 @@ const NewIssuePage = () => {
     <div className="max-w-xl space-y-3">
       <TextField.Input radius="large" placeholder="Issue title" />
       <TextArea variant="surface" size={"3"} placeholder="Issue description" />
-      <Button variant="solid">Submit Issue</Button>
+      <Button variant="solid" size={"3"}>
+        Submit New Issue
+      </Button>
     </div>
   );
 };
