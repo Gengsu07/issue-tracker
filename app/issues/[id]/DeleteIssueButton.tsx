947152040ddd6f800a3452e389e5d12876470c5f
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-10-22 22:02:01 +0700

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,48 +1,71 @@
 "use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
-import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, SetError] = useState(false);
+  const DeleteIssue = async () => {
+    try {
+      await axios.delete("/api/issue/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      SetError(true);
+    }
+  };
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">
-          <TrashIcon />
-          Delete Issue
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Delete Issue?</AlertDialog.Title>
-        <AlertDialog.Description>
-          Apakah yakin untuk menghapus Issue?.Issue yang sudah dihapus
-          <strong> tidak bisa dikembalikan</strong>
-        </AlertDialog.Description>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <TrashIcon />
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Delete Issue?</AlertDialog.Title>
+          <AlertDialog.Description>
+            Apakah yakin untuk menghapus Issue?.Issue yang sudah dihapus
+            <strong> tidak bisa dikembalikan</strong>
+          </AlertDialog.Description>
 
-        <Flex gap="3" mt="2">
+          <Flex gap="3" mt="2">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Batal
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={() => DeleteIssue()}>
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error On Deleting</AlertDialog.Title>
+          <AlertDialog.Description>
+            Oops, issue tidak berhasil dihapus.Coba lagi beberapa saat lagi...
+          </AlertDialog.Description>
           <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Batal
+            <Button
+              variant="soft"
+              color="gray"
+              mt="2"
+              onClick={() => SetError(false)}
+            >
+              OK
             </Button>
           </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={async () => {
-                await axios.delete("/api/issue/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
-            >
-              Delete Issue
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
