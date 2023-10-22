588cfc77b318b7c3475db9e903f42b15ed21df0c
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-10-22 17:04:44 +0700

    improve loading experie w lazzy form and skeleton

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import IssueForm from "../../_components/IssueForm";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
   params: { id: string };
@@ -13,7 +18,6 @@ const EditIssuePage = async ({ params }: Props) => {
   });
 
   if (!issue) notFound();
-
   return <IssueForm issue={issue} />;
 };
 
