72ef7aedc67d79e2745a091765d2e0c7b11da496
Gengsu07 <sugengdatascience@gmail.com>
2023-11-01 13:08:53 +0700

    Paginating issues

--- app/issues/_components/Pagination.tsx
+++ app/issues/_components/Pagination.tsx
@@ -1,11 +1,12 @@
+"use client";
 import {
-  ArrowLeftIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   DoubleArrowLeftIcon,
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -15,22 +16,46 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
   const totalPage = Math.ceil(itemCount / pageSize);
   return (
-    <Flex align="center" gap="2">
-      <Text>
+    <Flex align="center" gap="2" mt="2">
+      <Text size="2">
         Page {currentPage} of {totalPage}
       </Text>
-      <Button variant="soft" disabled={currentPage <= 2}>
+      <Button
+        variant="soft"
+        disabled={currentPage <= 2}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button variant="soft" disabled={currentPage === totalPage}>
+      <Button
+        variant="soft"
+        disabled={currentPage === totalPage}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button variant="soft" disabled={currentPage >= totalPage - 2}>
+      <Button
+        variant="soft"
+        disabled={currentPage >= totalPage - 2}
+        onClick={() => changePage(totalPage)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,9 +5,14 @@ import { IssueStatusBadge, LinkComponent } from "../../componets";
 import IssueAction from "./IssueAction";
 import { Issue, Status } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "../_components/Pagination";
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+  };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -26,17 +31,23 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const where = { status };
+
   const orderBy = columnConfig
     .map((col) => col.value)
     .includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
+  const currentPage = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+  const itemCount = await prisma.issue.count({ where });
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
+    where,
     orderBy,
+    skip: (currentPage - 1) * pageSize,
+    take: pageSize,
   });
 
   return (
@@ -85,6 +96,11 @@ const IssuesPage = async ({ searchParams }: Props) => {
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        currentPage={currentPage}
+        pageSize={20}
+        itemCount={itemCount}
+      />
     </div>
   );
 };
