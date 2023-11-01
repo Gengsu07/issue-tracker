b718e1ee7d13351df8a002a85826053ffd1fa2d4
Gengsu07 <sugengdatascience@gmail.com>
2023-11-01 19:29:03 +0700

    jangan lupa absen

---
+++ app/issues/list/IssuesTable.tsx
@@ -0,0 +1,64 @@
+import { LinkComponent, IssueStatusBadge } from "@/app/componets";
+import { Issue } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { Table } from "@radix-ui/themes";
+import NextLink from "next/link";
+import React from "react";
+
+const IssuesTable = () => {
+  const columnConfig: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Title", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+  return (
+    <div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            {columnConfig.map((col) => (
+              <Table.ColumnHeaderCell key={col.value} className={col.className}>
+                <NextLink
+                  href={{
+                    query: { ...searchParams, orderBy: col.value },
+                  }}
+                >
+                  {col.label}
+                </NextLink>
+                {col.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <LinkComponent href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </LinkComponent>
+                <div className="font-bold block mt-1 md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
+};
+
+export default IssuesTable;
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -16,16 +16,6 @@ interface Props {
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columnConfig: {
-    label: string;
-    value: keyof Issue;
-    className?: string;
-  }[] = [
-    { label: "Title", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
-
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
@@ -56,46 +46,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <div className="my-3">
         <IssueAction />
       </div>
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columnConfig.map((col) => (
-              <Table.ColumnHeaderCell key={col.value} className={col.className}>
-                <NextLink
-                  href={{
-                    query: { ...searchParams, orderBy: col.value },
-                  }}
-                >
-                  {col.label}
-                </NextLink>
-                {col.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <LinkComponent href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </LinkComponent>
-                <div className="font-bold block mt-1 md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+
       <Pagination
         currentPage={currentPage}
         pageSize={20}
